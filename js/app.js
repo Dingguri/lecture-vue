49c1f476f51b13710d9d10ef9c5f56411cd7180b
PARK SEUNG MIN <min62156347@gmail.com>
2020-02-27 23:34:30 +0900

    검색폼 구현
     x 버튼을 클릭하거나, 검색어를 삭제하였을 때, onReset 함수 호출

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ new Vue({
     methods: {
         onSubmit(e) {
 
+        },
+        onKeyup() {
+            if (!this.query.length) {
+                this.onReset()
+            }
+        },
+        onReset() {
+            this.query = ''
+            debugger
+            // todo : 검색 결과 숨김
         }
     }
 })
